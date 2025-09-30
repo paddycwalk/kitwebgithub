9f1b67633f07ca3b02dc81834006582d67d79deb
patrick.schott <patrick.schott@kittelberger.de>
2025-09-30 16:29:12 +0200

    [main] fix home page

--- apps/project1/app/page.tsx
+++ apps/project1/app/page.tsx
@@ -1,27 +1,21 @@
+// app/page.tsx
 import { StoryblokStory } from "@storyblok/react/rsc";
 import { getStoryblokApi } from "../lib/storyblok";
 
-export default async function HomeWrapper() {
-  return <HomeAsync />;
-}
-
-async function HomeAsync() {
+export default async function HomePage() {
   const storyblokApi = getStoryblokApi();
 
   try {
-    const { data } = await storyblokApi.get(`cdn/stories/`, {
+    const { data } = await storyblokApi.get(`cdn/stories/home`, {
       version: "draft",
     });
-    const story = data.stories?.[0]; // Prüfen, ob Stories existieren
+    const story = data.story;
 
-    if (!story) {
-      return <div>Keine Story vorhanden</div>;
-    }
+    if (!story) return <div>Home nicht gefunden</div>;
 
     return <StoryblokStory story={story} />;
   } catch (err) {
-    console.error("Fehler beim Laden der Story:", err);
-
-    return <div>Fehler beim Laden der Story</div>;
+    console.error("Fehler beim Laden der Home-Story:", err);
+    return <div>Fehler beim Laden der Home-Seite</div>;
   }
 }
